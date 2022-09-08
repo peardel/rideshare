37d6663e1cd12689d187e69a1dde2eebab56e683
Adam <peardel59@gmail.com>
2022-09-08 20:06:14 +0100

    test something

--- tests.py
+++ tests.py
@@ -2,7 +2,7 @@ import student
 
 def try_distance():
     pupil1 = student.Pupil("GU1 1SZ", False, False, 1)
-    pupil2 = student.Pupil("RH10 7PC", False, False, 1)
+    pupil2 = student.Pupil("KT11 2AF", False, False, 1)
 
     print(pupil1.get_distance(pupil2))
 
