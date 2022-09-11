1d72403e5eb7d25ca453dfff6efc7f191f9792d6
Adam <peardel59@gmail.com>
2022-09-11 20:30:38 +0100

    not broken but just doesn't work

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pandas
 import openrouteservice
 import original_dataset_handler
+import asyncio
 from pupil_classes import Pupil
 
 df = pandas.read_excel("OriginalDataset.xlsx")
 
 coords = lambda A,B: ((A.longitude,A.latitude),(B.longitude,B.latitude))
 
-SCHOOL = Pupil(-1, "SCHOOL", "GU1 3BB", False, False, 0)
 MAX_DISTANCE_TRAVELLABLE = 0.1
 client = openrouteservice.Client(key="5b3ce3597851110001cf6248b0c4037940f3452a988853816da080e9") # distance calcs should only be used to go to school, not to compare with other students
 
@@ -15,6 +15,10 @@ def get_distance_to_school(pupil: Pupil):
     routes = client.directions(coords(pupil, SCHOOL))
     return routes["routes"][0]["summary"]["distance"]
 
-pupils, pupils_willing_to_drive = original_dataset_handler.harvest_pupil_data_from_excel()
-for pupil in pupils:
-    print(pupil)
+async def main():
+    SCHOOL = Pupil(-1, "SCHOOL", "GU1 3BB", False, False, 0, (0,0))
+    pupils, pupils_willing_to_drive = await original_dataset_handler.harvest_pupil_data_from_excel()
+    for pupil in pupils:
+        print(pupil)
+
+asyncio.run(main())
