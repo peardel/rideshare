d66e0492a1e4d9115d5f4d6aa41dc2e61e8a7c71
Adam <peardel59@gmail.com>
2022-09-11 20:56:58 +0100

    stupid

--- pupil_group_calculator.py
+++ pupil_group_calculator.py
@@ -5,6 +5,7 @@ import tools
 import random
 from enum import IntEnum
 import math
+import itertools
 
 class ArrivalOrDeparture(IntEnum):
     ARRIVAL = 0
@@ -47,10 +48,11 @@ def seperate_pupils_by_times(pupils: list[pupil_classes.Pupil], day: int, arriva
         print(f"\t{tools.int_to_time_string(time)}: [{', '.join(f'#{str(pupil.id).zfill(2)}' for pupil in pupils_in_section)}]")
     return pupil_sections
 
-def create_random_groups(pupils: list[pupil_classes.Pupil]) -> list[list[pupil_classes.Pupil]]:
+def create_random_groups(pupils: list[pupil_classes.Pupil], verbosity:int=2) -> list[list[pupil_classes.Pupil]]:
+    if verbosity >= 1: print(f"Creating random groups with pupils {', '.join(f'#{str(p.id)}' for p in pupils)}")
     # nominate random group drivers till we run out people
     groups: "list[Group]" = []
-    pupils_willing_to_share_and_join = [pupil for pupil in pupils if pupil.will_share_others]
+    pupils_willing_to_share_and_join = [pupil for pupil in pupils if pupil.will_share_others and pupil.will_join_others]
     pupils_willing_to_share_only = [pupil for pupil in pupils if pupil.will_share_others and not pupil.will_join_others]
     pupils_freeloading = [pupil for pupil in pupils if not pupil.will_share_others]
     # all these lists above are mutually exclusive
@@ -72,5 +74,70 @@ def create_random_groups(pupils: list[pupil_classes.Pupil]) -> list[list[pupil_c
     for pupil in pupils_willing_to_share_and_join:
         # see which group is best to add them to
         best_dist = math.inf
+        best_group = None
         for group in [group for group in groups if group.can_take_another_pupil()]:
-            dist = group.query_route_extension_if_add_new_pupil()
+            dist = group.query_route_extension_if_add_new_pupil(pupil)
+            if dist < best_dist:
+                dist = best_dist
+                best_group = group
+        if best_group is None or best_dist > pupil.distance_to_school:
+            # make a new group with this person
+            groups.append(Group(pupil))
+        else:
+            best_group.add_pupil(pupil)
+
+    for group in groups:
+        if verbosity >= 2: print(f"\tGroup with driver #{group.driver.id}: [{', '.join(f'#{str(p.id)}' for p in group.people)}] (max {group.driver.spare_seats}) ({'wants' if group.driver.will_share_others else 'doesnt want'} to share) ({'wants' if group.driver.will_join_others else 'doesnt want'} to join)")
+
+    return groups
+
+def group_improvement_attempt_reorder(groups: list[Group], verbose=False) -> tuple[list[Group], bool]:
+    if verbose: print(f"Attempting to reorder groups [{',  '.join(f'#{g.driver.id}' for g in groups)}]")
+    made_an_improvement = False
+    for group in groups:
+        # try to shuffle the group and see what happens
+        if len(group.people) == 1: continue
+        original_route_length = group.route_length
+        best_new_route_length = original_route_length
+        new_perm = None
+        for perm in itertools.permutations(group.people):
+            if perm == group.people: continue # this is the original
+            if not perm[0].will_share_others: continue # the driver doesn't want to share
+            if perm[0].spare_seats+1 < len(group.people): continue # the driver doesn't have enough seats
+            everyone_riding_wants_to_join = True
+            for person in perm[1:]:
+                if not person.will_join_others:
+                    everyone_riding_wants_to_join = False
+                    break
+            if not everyone_riding_wants_to_join: continue # there are passengers who dont want to join
+            # ok, then calculate the new length of the route
+            new_route_length = 0
+            for i in range(len(group.people) - 1):
+                new_route_length += perm[i].get_distance_to_other_pupil(perm[i+1])
+            new_route_length += perm[-1].distance_to_school
+            if new_route_length < best_new_route_length:
+                new_perm = perm
+                best_new_route_length = new_route_length
+
+        if new_perm is not None:
+            # we have found a better ordering of everyone
+            made_an_improvement = True
+            print(f"Group went from [{', '.join(f'#{p.id}' for p in group.people)}] with length {group.route_length}, to: ", end="")
+            group.people = new_perm
+            group.route_length = best_new_route_length
+            print(f"[{', '.join(f'#{p.id}' for p in group.people)}] with length {group.route_length}")
+
+    return groups, made_an_improvement
+
+def group_improvement_attempt_remove_into_other_group(groups: list[Group], iterations:int = 100, verbose=False) -> tuple[list[Group], bool]:
+    made_improvement = False
+    for iteration in iterations:
+        # pick a random group
+        groups_with_passengers = [g for g in groups if len(g) > 1]
+        if len(groups_with_passengers) == 0:
+            return groups, made_improvement
+        main_group = random.choice(groups_with_passengers)
+        for i, person in enumerate(main_group):
+            if i == 0: continue # dont remove driver
+
+    # WIP
--- tests.py
+++ tests.py
@@ -1,6 +1,7 @@
 import pupil_classes
 import original_dataset_handler
 import pupil_group_calculator
+import asyncio
 
 def try_distance():
     pupil1 = pupil_classes.Pupil("GU1 1SZ", False, False, 1)
@@ -8,10 +9,10 @@ def try_distance():
 
     print(pupil1.get_distance(pupil2))
 
-def try_pupil_time_seperation():
-    pupils, pupils_willing_to_share = original_dataset_handler.harvest_pupil_data_from_excel(get_pos_with_api=True)
+async def try_pupil_time_seperation():
+    pupils, pupils_willing_to_share = await original_dataset_handler.harvest_pupil_data_from_excel(get_pos_with_api=True)
     times_dict = pupil_group_calculator.seperate_pupils_by_times(pupils, 0, pupil_group_calculator.ArrivalOrDeparture.ARRIVAL)
     for time, pupils in times_dict.items():
         groups = pupil_group_calculator.create_random_groups(pupils)
 
-try_pupil_time_seperation()
+asyncio.run(try_pupil_time_seperation())
